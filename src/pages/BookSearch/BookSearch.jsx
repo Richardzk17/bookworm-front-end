9f4286670a10b6a4349784c00bffd3408e640d31
richardzk17 <richardhernandezk17@gmail.com>
2024-02-12 23:39:57 -0500

    add style to bookdetails

--- src/pages/BookSearch/BookSearch.jsx
+++ src/pages/BookSearch/BookSearch.jsx
@@ -14,10 +14,37 @@ const BookSearch = () => {
       let newKey = key.split("/")
       return newKey[newKey.length - 1]
     }
-    
+
     return ( 
         <div className="container">
         <div className="content">
+        <div>
+      <Search term={searchTerm} searchKeyword={setSearchTerm} />
+      {loading && <p>Loading Books...</p>}
+      <ul>
+        {books.slice(0, 10).map((book, index) => {
+          return (
+            <li key={index}>
+              <Link to={`/book/${getIdOfBook(book.key)}`}>
+                <p>Title: {book.title}</p>
+              </Link>
+              {book.author_name && (
+                <p>Author: {book.author_name.join(", ")}</p>
+              )}
+              {book.first_publish_year && (
+                <p>First Publish Year: {book.first_publish_year}</p>
+              )}
+              <img
+                src={getCoverUrl(book)}
+                alt={`Cover of ${book.title}`}
+                style={{ width: "150px", height: "auto" }}
+              />
+            </li>
+          )
+        }
+        )}
+      </ul>
+    </div>
 
         </div>
         </div>
@@ -25,3 +52,13 @@ const BookSearch = () => {
 }
  
 export default BookSearch;
+
+export const getCoverUrl = (book) => {
+    if (book.cover_i) {
+      return `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`;
+    }
+    return "https://via.placeholder.com/150";
+  };
+  
+  
+  
