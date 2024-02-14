0297df544b0248437ae9e9bc63aebdddd5067754
joshuabutler <butlerja.nhs@gmail.com>
2024-02-13 22:15:36 -0500

    edit profile card and all profiles page

--- src/components/ProfileCard/ProfileCard.jsx
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -1,9 +1,36 @@
+// npm modules 
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+
+// services 
+import * as profileService from '../../services/profileService'
+
+// css 
+import styles from './ProfileCard.module.css'
 
 
 const ProfileCard = () => {
+  const [profiles, setProfiles] = useState([])
+  
+
+  useEffect(() => {
+    const fetchProfiles = async () => {
+      const profileData = await profileService.getAllProfiles()
+      setProfiles(profileData)
+    }
+    fetchProfiles()
+  }, [])
+
+  if (!profiles.length) {
+    return <main className={styles.container}><h1>Sorry! I was reading!</h1></main>
+  }
+
+
   return ( 
     <>
       <h2>hello!</h2>
+      <div>
+        </div>
     </>
   )
 }
--- src/pages/Profiles/Profiles.jsx
+++ src/pages/Profiles/Profiles.jsx
@@ -1,37 +1,17 @@
-// npm modules
-import { useState, useEffect } from 'react'
-
-// services
-import * as profileService from '../../services/profileService'
-
 // components
 import ProfileCard from '../../components/ProfileCard/ProfileCard'
 // css
 import styles from './Profiles.module.css'
 
-const Profiles = () => {
-  const [profiles, setProfiles] = useState([])
-
-  useEffect(() => {
-    const fetchProfiles = async () => {
-      const profileData = await profileService.getAllProfiles()
-      setProfiles(profileData)
-    }
-    fetchProfiles()
-  }, [])
-
-  if (!profiles.length) {
-    return <main className={styles.container}><h1>Loading...</h1></main>
-  }
+const Profiles = (props) => {
   
   return (
     <div className={styles.container}>
       <div className={styles.content}>
         <h1>All Bookworm Profile Users</h1>
-        {profiles.map(profile => (
-          <p key={profile._id} className={styles.profile}>{profile.name}
-          <ProfileCard /></p>
-        ))}
+        <div>
+          {Profiles.map}
+        </div>
       </div>
     </div>
   )
