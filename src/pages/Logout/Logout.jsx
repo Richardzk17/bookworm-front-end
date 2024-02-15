2172bdfb759784f7632d51702739347755824012
richardzk17 <richardhernandezk17@gmail.com>
2024-02-14 21:55:35 -0500

    add stylr to logout page

--- src/pages/Logout/Logout.jsx
+++ src/pages/Logout/Logout.jsx
@@ -11,6 +11,7 @@ const Logout = () => {
         <h1>You are Logged Out</h1>
         <p className={styles.text}>Thank you for using Bookworm</p>
         <Link className={styles.signIn} to="/auth/login">Sign in</Link>
+    
       </div>
     </div>
   )
