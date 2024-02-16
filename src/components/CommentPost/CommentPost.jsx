4499cb00e322510251fbbed4efbd6081814d70f8
richardzk17 <richardhernandezk17@gmail.com>
2024-02-16 08:55:19 -0500

    fix timestamp

--- src/components/CommentPost/CommentPost.jsx
+++ src/components/CommentPost/CommentPost.jsx
@@ -1,17 +1,22 @@
 import AuthorInfo from "../AuthorInfo/AuthorInfo"
+import styles from "../CommentPost/CommentPost.module.css"
 
 const CommentPost = (props) => {
   return (
     <article>
-      <header>
-        <AuthorInfo content={props.comment}/>
+      <header className={styles.header}>
+        <AuthorInfo content={props.comment} />
+        <span className={styles.timestamp}>
+          {new Date(props.comment.createdAt).toLocaleDateString()} @ {new Date(props.comment.createdAt).toLocaleTimeString()}
+        </span>
       </header>
       <p>{props.comment.text}</p>
       {props.comment.author._id === props.user.profile &&
-        <button onClick={() => props.handleDeleteComment(props.comment._id)}>Delete</button>
+        <button className={styles.deleteBtn} onClick={() => props.handleDeleteComment(props.comment._id)}>Delete</button>
       }
     </article>
   )
 }
 
-export default CommentPost
+export default CommentPost;
+
